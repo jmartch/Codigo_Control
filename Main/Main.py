bdfbe3e2c5fc2c02f17066086f8b0635d27abe58
jmartch <juanangelm@uninorte.edu.co>
2025-02-18 10:50:17 -0500

    1. Isin 2. Ean 3. Isbn
    Punto 1 ,2 y 3

--- Main/Main.py
+++ Main/Main.py
@@ -2,82 +2,96 @@
 #Juanangel Martinez
 #Vivian Buelvas
 #Librerias
-from idlelib.configdialog import is_int
-import re
-import isbnlib as isbn
 from isbnlib import is_isbn10
-import pandas as pd
-#Variables ciclo iterativo
-w = False
 
-while(w != True):
-    print("Bienvenido a DecodeTalker")
-    opcion = int(input("Opciones:\n "
-          "1. Verificacion ISBN-10\n "
-          "2. Verificacion EAN-13\n"
-          "3. Verificacion ISIN\n"
-          "4. Deteminar codigo de control\n"
-          "5. Salir\n"))
+#Funciones
+# Documentacion funcion is_isbn-10
+# https://pypi.org/project/isbnlib/
+# Codigo ejemplo de como funciona de la misma libreria
+# is_isbn10(isbn10like)
+# Validates as ISBN-10.
+# import the core functions you need
+# from isbnlib import canonical, is_isbn10, is_isbn13
 
-    if (opcion == 1):
-        codigo= int(input("Ingrese el Codigo "))
+# isbn = canonical("978-0446310789")
+# if is_isbn13(isbn):
+#    ...
+# ...
+# Función para verificar si un código es un EAN-13
 
-        if(is_isbn10(codigo)):
-            print("El codigo ingresa es un EAN-13")
+def is_ean13(codigo):
+    if not codigo.isdigit() or len(codigo) != 13:
+        return False
+
+    even = 0
+    odd = 0
+    check_bit = int(codigo[-1])
+    check_val = codigo[:-1]
+
+    for i, n in enumerate(check_val):
+        if i % 2 == 0:
+            even += int(n)
         else:
-            print("El codigo ingresa NO es un EAN-13")
-    if (opcion == 2):
-        codigo = int(input("Ingrese el Codigo "))
-        is_ean13(codigo)
-    if (opcion == 3):
-        codigo = int(input("Ingrese el Codigo "))
-        if (identificador(codigo)):
-            print("El codigo ingresa es un ISBN-10")
-        else:
-            print("El codigo ingresa NO es un ISIN")
-    if (opcion == 4):
-        codigo = int(input("Ingrese el Codigo a resolver "))
-        c = identificador(codigo)
-    if (opcion == 5):
-        w = True
+            odd += int(n)
 
-    def is_ean13(codigo):
-        ean=codigo
-        even = 0
-        odd = 0
-        check_bit = ean[len(ean) - 1]  # get check bit(last bit)
-        check_val = ean[:-1]  # Get all vals except check bit
+    return ((3 * odd) + even + check_bit) % 10 == 0
 
-        if len(ean) != 13:  # Check the input length
-            print("Invalid EAN 13")
+
+def is_isin(codigo):
+    sum = 0
+    abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    k1 = str(codigo[0])
+    k2 = str(codigo[1])
+
+    isin = str(int(abc.index(k1) + 10)) + str(int(abc.index(k2) + 10)) + codigo[2:]
+
+    for i in range(1,15):
+        if i % 2 == 0:
+            sum += int(isin[i-1])
         else:
-            for index, num in enumerate(check_val):  # Gather Odd and Even Bits
-                if (index % 2 == 0):
-                    even += int(num)
-                else:
-                    odd += int(num)
-            if (((3 * odd) + even + int(check_bit)) % 10 == 0):  # Check if the algorithm 3 * odd parity + even parity + check bit matches
-                print("Valid EAN 13")
+            if int(isin[i-1]) * 2 >= 10 :
+                sum += (int(isin[i-1]) * 2) // 10 +  (int(isin[i-1])*2) % 10
             else:
-                print("Invalid EAN 13")
+                sum += (int(isin[i-1])) * 2
 
-    def is_isin(codigo):
-        # Regex to check valid ISIN Code
-        regex = "^[A-Z]{2}[-]{0, 1}[0-9A-Z]{8}[-]{0, 1}[0-9]{1}$"
+    if sum % 10 == 0:
+        return True
+    else:
+        return False
 
-        # Compile the ReGex
-        p = re.compile(regex)
+#Variables ciclo iterativo
+w= False
+while not w:
+    print("\nBienvenido a DecodeTalker")
+    opcion = input("Opciones:\n"
+                   "1. Verificación ISBN-10\n"
+                   "2. Verificación EAN-13\n"
+                   "3. Verificación ISIN\n"
+                   "4. Determinar código de control\n"
+                   "5. Salir\n")
 
-        # If the string is empty
-        # return false
-        if (codigo == None):
-            return False
+    if opcion == "1":
+        codigo = input("Ingrese el Código: ")
+        print("El código ingresado es un ISBN-10" if is_isbn10(codigo) else "El código ingresado NO es un ISBN-10")
 
-        # Return if the string
-        # matched the ReGex
-        if (re.search(p, codigo)):
-            return True
-        else:
-            return False
+    elif opcion == "2":
+        codigo = input("Ingrese el Código: ")
+        print("El código ingresado es un EAN-13" if is_ean13(codigo) else "El código ingresado NO es un EAN-13")
 
-    #def resolver(codigo):
+    elif opcion == "3":
+        codigo = input("Ingrese el Código: ")
+        codigo_g = codigo.upper()
+        is_isin(codigo_g)
+        print("El código ingresado es un ISIN" if is_isin(codigo) else "El código ingresado NO es un ISIN")
+
+    elif opcion == "4":
+        codigo = input("Ingrese el Código a resolver: ")
+        print(f"El dígito de control es: {calcular_digito_control(codigo)}")
+
+    elif opcion == "5":
+        w = True
+        print("Gracias por usar DecodeTalker. ¡Hasta luego!")
+    else:
+        print("Opción no válida. Intente nuevamente.")
+
+
